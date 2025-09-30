8653f07ec01eaecc74477247281a7430382db508
Brian Phan <40909481+bachsofttrick@users.noreply.github.com>
2025-09-29 22:50:42 -0700

    Update prompt_tech.py

--- prompt_tech.py
+++ prompt_tech.py
@@ -2,25 +2,33 @@ import os
 
 os.system('clear')
 ogprompt = '''SKILLS:
-• Frontend: React, Vue, PHP. Backend: .NET, NodeJS, NestJS, ExpressJS, PHP
-• Database: MySQL, MongoDB, SQLite, Redis
-• Scripting: Bash, Python. Version Control: Git, GitHub, GitLab
-• Container & Orchestration: Docker, Rancher. Virtualization: KVM, VPS
-• Proficient in Linux (Ubuntu, Debian, Fedora, Arch), Windows
-• Experience in AWS, Azure
+- Language: C#, Python, HTML, CSS, Javascript, PHP, Bash
+- Frontend: React, Vue
+- Backend: .NET, NodeJS, NestJS, ExpressJS, Laravel
+- Database: MySQL, MongoDB, SQLite, Redis
+- Tools: Git, Docker, Rancher
+- Virtualization: KVM, VPS
+- Systems: Linux, Windows
 Experience:
 OSU COLLEGE OF AGRICULTURAL SCIENCES 9/2025
-• Managed and maintained the OregonFlora codebase and infrastructure to ensure reliability and long-term usability.
+• Managed and maintained the OregonFlora codebase and infrastructure to ensure reliability and long-term usability  for researchers.
 OSU EXTENSION SERVICE OUTDOOR SCHOOL 9/2024 – 10/2024
 • Maintenance on the portal through which school districts and teachers can access the application, reporting systems, resources for developing outdoor school programs.
 • Technology migration: From Umbraco 8 to Drupal 10 to improve stability, performance.
 • Supported website content updates, maintaining consistency and clarity across pages.
 ICC INNOVATIVE CONSULTING 11/2020 – 11/2022
-• Maintained a distribution management system for Business-to-business sales. Specialized in promotion/deal calculation.
-• Made improvements to the activity generation algorithm that sped up by 96x.
-• Programmed a social security app offering relief packages during COVID-19 (An Sinh), utilized by about 9 million citizens in HCMC.
+- Maintained and optimized a B2B distribution management system,
+focusing on promotions and deal calculation accuracy.
+- Enhanced the activity generation algorithm, achieving a 96x speedup in runtime efficiency.
+- Developed and deployed An Sinh, a large-scale social security relief application
+supporting 9 million citizens during COVID-19 in Ho Chi Minh City.
+Projects:
+REVERSI: MINIMAX VS MONTE CARLO
+• Designed and compared performance of two AI algorithms against several games of Reversi, varying in metrics using multithreading.
+OBJECT DETECTION AND MONITORING THROUGH UAV
+• Engineered a custom drone for searching, identifying, and following a designated subject while transmitting live video feeds to any RTSP-compatible device.
 Summary of my resume:
-Full stack web developer with experience in designing, deploying, and maintaining web applications across the full stack. Skilled in JavaScript, React, MySQL, CSS, and HTML, with a strong background in Linux systems and Agile development. Experienced in managing deadlines, collaborating with cross-functional teams, and delivering solutions that improve functionality and user experience. Adept at documentation, content management, and supporting site updates. Committed to building efficient, user-friendly tools and contributing to the growth of mission-driven projects.
+Full stack web developer with experience in designing, deploying, and maintaining web applications. Skilled in .NET, JavaScript, React, MySQL, Docker, with a strong background in Linux systems and Agile development. Committed to building efficient, user-friendly tools and contributing to the growth of mission-driven projects.
 '''
 with open('prompt.txt', 'r', encoding='utf-8') as f:
     job = f.read()

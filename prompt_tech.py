4d85870bce3b8894b8815c27a427a90e76ae7b9f
bachsofttrick <xuanbach1307@gmail.com>
2025-10-15 22:09:52 -0700

    update prompt for new resume

--- prompt_tech.py
+++ prompt_tech.py
@@ -11,24 +11,19 @@ ogprompt = '''SKILLS:
 - Systems: Linux, Windows
 Experience:
 OSU COLLEGE OF AGRICULTURAL SCIENCES 9/2025
-• Managed and maintained the OregonFlora codebase and infrastructure to ensure reliability and long-term usability  for researchers.
+Maintained and enhanced a research platform supporting thousands of daily users, researchers, and conservationists across Oregon.
 OSU EXTENSION SERVICE OUTDOOR SCHOOL 9/2024 – 10/2024
 • Maintenance on the portal through which school districts and teachers can access the application, reporting systems, resources for developing outdoor school programs.
-• Technology migration: From Umbraco 8 to Drupal 10 to improve stability, performance.
-• Supported website content updates, maintaining consistency and clarity across pages.
+• Supported migration from Umbraco 8 to Drupal 10, contributing to improved platform stability and performance.
 ICC INNOVATIVE CONSULTING 11/2020 – 11/2022
-- Maintained and optimized a B2B distribution management system,
-focusing on promotions and deal calculation accuracy.
-- Enhanced the activity generation algorithm, achieving a 96x speedup in runtime efficiency.
-- Developed and deployed An Sinh, a large-scale social security relief application
-supporting 9 million citizens during COVID-19 in Ho Chi Minh City.
+- Maintained and optimized a B2B distribution management system, focusing on promotions and deal calculation accuracy.
+- Enhanced the activity generation algorithm, reducing runtime from 1 hour to 1 minute.
+- Developed and deployed An Sinh, a large-scale social security relief application supporting 9 million citizens during COVID-19 in Ho Chi Minh City.
 Projects:
 REVERSI: MINIMAX VS MONTE CARLO
 • Designed and compared performance of two AI algorithms against several games of Reversi, varying in metrics using multithreading.
 OBJECT DETECTION AND MONITORING THROUGH UAV
 • Engineered a custom drone for searching, identifying, and following a designated subject while transmitting live video feeds to any RTSP-compatible device.
-Summary of my resume:
-Full stack web developer with experience in designing, deploying, and maintaining web applications. Skilled in .NET, JavaScript, React, MySQL, Docker, with a strong background in Linux systems and Agile development. Committed to building efficient, user-friendly tools and contributing to the growth of mission-driven projects.
 '''
 with open('prompt.txt', 'r', encoding='utf-8') as f:
     job = f.read()

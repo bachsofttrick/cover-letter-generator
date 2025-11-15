9c45a8ab3477f488a2bdd5127cbf52275b29c633
bachsofttrick <xuanbach1307@gmail.com>
2025-11-15 13:13:41 -0800

    remove prompt for non-tech, update prompt for tech

--- prompt.py
+++ prompt.py
@@ -1,33 +1,38 @@
 import os
 
 os.system('clear')
-ogprompt = '''SKILLS:
-• Comfortable managing deadlines and timelines across concurrent projects.
-• Capable of working independently and with teams on long- and short-term tasks.
-• Proficient in Microsoft Office Suite (Word, Excel, PowerPoint)
-• Experience with data entry and maintaining databases.
-• Effective verbal and written communicator with attention to detail.
-• Multilingual: English, Vietnamese
-• Experience with digitization of files and information management systems.
-Experience:
-OSU CARLSON COLLEGE OF VETERINARY MEDICINE 7/2025 – 8/2025
-• Led and supervised a group of academically driven high school students during OSU's Summer Veterinary Experience, ensuring their safety, engagement, and successful participation in a smooth, incident-free program.
-• Promoted a supportive, inclusive environment by building rapport with students from diverse backgrounds, contributing to a positive group and increased interest in veterinary careers.
-OSU EXTENSION SERVICE OUTDOOR SCHOOL 9/2024 – 10/2024
-• Supported website content updates, maintaining consistency and clarity across pages.
-• Collaborated with team members to process content and image submissions.
-• Organized files and contributed to documentation efforts for site management.
-ICC INNOVATIVE CONSULTING 11/2020 – 11/2022
-• Collaborated on development teams to design and deploy full-stack web applications.
-• Managed timelines, deadlines, and client deliverables using Agile methodologies.
-• Produced documentation, reports, and spreadsheets to support team operations.
-Summary of my resume:
-Detail-oriented and adaptable student with strong organizational, communication, and computer skills. Experienced in supporting office operations, managing files and databases, and assisting with content creation and digitization projects. Proficient in Microsoft Office Suite and comfortable with email, phone, and in-person communication. Skilled at working independently or with teams to complete tasks efficiently, troubleshoot technical issues, and provide excellent support for faculty, staff, and students.
+resume = '''WORK EXPERIENCE
+Software Developer	9/2025 - present
+OregonFlora - Corvallis, Oregon
+• Maintained and enhanced a research platform supporting thousands of daily users in Oregon.
+• Improved the uploading process, reducing runtime from 2 hour to 2 minute.
+Web Assistant	9/2024 - 10/2024
+OSU Extension Service - Outdoor School - Corvallis, Oregon
+• Maintained the portal through which school districts and teachers can access the application, reporting systems, resources for developing outdoor school programs.
+• Supported platform migration, contributing to improved stability and performance.
+Software Developer	9/2020 - 11/2022
+ICC Innovative Consulting - Ho Chi Minh City, Vietnam
+• Maintained and optimized a business-to-business distribution management system, focusing on promotions and deal calculation accuracy.
+• Enhanced the activity generation algorithm, reducing runtime from 1 hour to 1 minute.
+• Developed and deployed An Sinh, a large-scale social security relief application supporting 9 million citizens during COVID-19 in Ho Chi Minh City.
+PROJECTS
+Reversi: Minimax VS Monte Carlo - Paper
+• Designed and compared performance of two AI algorithms against several games of Reversi, varying in configurations using multithreading.
+Object Detection and Monitoring through UAV - Paper | Video 1 | Video 2
+• Engineered a custom drone for searching, identifying, and following a designated subject while transmitting live video feeds to any RTSP-compatible device.
+SKILLS
+• Language: C, C++, C#, Python, Go, Javascript, TypeScript, SQL, NoSQL, Bash
+• Frontend: React, Vue, Svelte, NextJS, Tailwind, Bootstrap, SCSS
+• Backend: .NET, NodeJS, NestJS, ExpressJS, FastAPI, Django, Laravel, Ruby on Rails
+• Database: MySQL, Postgres, MongoDB, Redis, Kafka, Solr, ElasticSearch
+• CI/CD Tools: Git, Github Actions, Docker, Kubernetes, Ansible
+• AI Developer Tools: Copilot, Cursor, Claude Code, ChatGPT
+• Cloud Platforms: AWS
 '''
 with open('prompt.txt', 'r', encoding='utf-8') as f:
     job = f.read()
 
-prompt = ogprompt + 'Use the preceeding information to write me a cover letter so it aligns with this job:\n' + job
+prompt = 'Act like a hiring manager for this role in this description:\n' + job + '\nNow I got this job. Use points in my old resume before this job, craft a resume to fit in company\'s misson, culture that got me this job:\n' + resume
 print(prompt)
 if False:
 	print('----- Press Enter -----')

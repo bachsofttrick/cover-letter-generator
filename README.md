5ffc79882a0d4754a05357ea26a335b11d0cd525
bachsofttrick <xuanbach1307@gmail.com>
2026-01-17 16:05:07 -0800

    add error catch and health check for llama

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ An AI-powered program that generates cover letter based on the job description.
 Todo:
 - [x] Implement REST API of llama.cpp to incorporate LLM in.
 - [x] Make a prompt that ONLY releases result, then print out the result.txt
-- [ ] Add some error handling to prevent 404, wrong url, fetching the wrong file...
+- [x] Add some error handling to prevent 404, wrong url, fetching the wrong file...
 - [ ] Implement llama-cpp-python to incorporate LLM in, since the lib is perfect for text prompt.
-  - [ ] Implement an if case to check REST API first (/health).
+  - [x] Implement an if case to check REST API first (/health).
 - [ ] Use LangChain (if possible) to split the task to:
   - [ ] Get the keywords of the job
   - [ ] Make a perfect resume based on the job description (as a reference only)
--- prompt.py
+++ prompt.py
@@ -5,38 +5,70 @@ from openai import OpenAI
 # Global config
 resumeUrl = "https://raw.githubusercontent.com/bachsofttrick/bachsofttrick.github.io/refs/heads/main/app/about/resume.md"
 gptUrl = "http://localhost:8033/v1"
+healthCheckUrl = gptUrl + "/health"
+jobFile = "job.txt"
+resultFile = "result.txt"
 
-# Get resume from portfolio
-res = requests.get(resumeUrl)
-resume = res.text
+def main():
+    # llama.cpp health check
+    try:
+        with requests.get(healthCheckUrl) as res:
+            if res.status_code != 200 or res.json()["status"] != "ok":
+                raise Exception(res.json())
+    except Exception as e:
+        print(f"Something is wrong with llama.cpp.\n{e}")
+        return
 
-# Get prompt
-with open('prompt.txt', 'r', encoding='utf-8') as f:
-    job = f.read()
-prompt = 'This is the job description:\n' + job + 'This is my resume:\n' + resume + '''
-Only use the WORK EXPERIENCE (ignore the internship), SKILLS sections in my resume.
-Write a concise, engaging, and professional cover letter with no bold style used.
-Starting with "Dear".
-'''
+    print("Getting resume from portfolio...")
+    try:
+        with requests.get(resumeUrl) as res:
+            if res.status_code != 200 or "---\ntitle" not in res.text:
+                raise Exception('Wrong data')
+            resume = res.text
+    except Exception as e:
+        print(f'Resume Not Found:\n{e}')
+        return
 
-# Running to GPT to write
-messages = [
-    {
-        "role": "user",
-        "content": prompt
-    }
-]
+    print("Reading job description...")
+    try:
+        with open(jobFile, 'r', encoding='utf-8') as f:
+            job = f.read()
+    except Exception as e:
+        print(f"Error while reading prompt:\n{e}")
+        return
+    
+    prompt = 'This is the job description:\n' + job + 'This is my resume:\n' + resume + '''
+    Only use the WORK EXPERIENCE (ignore the internship), SKILLS sections in my resume.
+    Write a concise, engaging, and professional cover letter with no bold style used.
+    Starting with "Dear".
+    '''
 
-print("Writing cover letter...")
-client = OpenAI(
-    base_url=gptUrl,
-    api_key=''
-)
-result = client.chat.completions.create(model='', messages=messages)
+    # Running to GPT to write
+    messages = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
 
-# Get the result and shave off the newline
-result = result.choices[0].message.content[2:]
-with open('result.txt', 'w', encoding='utf-8') as f:
-    f.write(result)
+    print("Writing cover letter...")
+    client = OpenAI(
+        base_url=gptUrl,
+        api_key=''
+    )
 
-print('Finished writing.')
+    try:
+        result = client.chat.completions.create(model='', messages=messages)
+
+        # Get the result and shave off the newline
+        result = result.choices[0].message.content[2:]
+        with open(resultFile, 'w', encoding='utf-8') as f:
+            f.write(result)
+
+        print('Finished writing.')
+    except Exception as e:
+        print(f"Error while writing:\n{e}")
+        return
+
+if __name__ == "__main__":
+    main()

c08ba590a70467d19635f0e390277001e77a61f4
bachsofttrick <xuanbach1307@gmail.com>
2026-06-27 17:14:02 -0700

    update wage file

--- yearwagecalc.py
+++ yearwagecalc.py
@@ -5,7 +5,7 @@ hourly_rate = float(input("Enter your hourly rate: "))
 hours_per_day = 8
 days_per_week = 5
 weeks_per_month = 4
-months_per_year = 12
+months_per_year = 13
 
 # Calculate annual salary
 annual_salary = hourly_rate * hours_per_day * days_per_week * weeks_per_month * months_per_year
